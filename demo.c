c17f3fc0b325f6781bc86d06af372a4075a52d4b
Anton-Leonov-IKBO-03-17 <oh22@mail.ru>
2017-10-06 16:12:56 +0300

    Update demo.c

--- demo.c
+++ demo.c
@@ -2,26 +2,26 @@
 #include <time.h>
 int main()
 {
-    int mas [6][5];
+    int mas [5][6];
     int i,j,tmp1, tmp2;
     srand(time(NULL));
     
-    for (i=0; i<6; i++){
-        for (j=0; j<5; j++){
+    for (i=0; i<5; i++){
+        for (j=0; j<6; j++){
             mas[i][j]=rand()%100;
             printf("%d  ", mas[i][j]);
         }
     printf("\n");
     }
     
-    for (i=0; i<6; i++){
-        for (j=0; j<3; j++){
-            tmp1 = mas [0][j/2];
-            mas [0][j/2] = mas [5][6-(j/2)];
-            mas [5][6-(j/2)] = tmp1;
-        }
+    for (i=0, j = 5; i<3; i++, j--){
+        tmp1 = mas [0][i];
+        mas [0][i] = mas [0][j];
+        mas [0][j] = tmp1;
     }
+    
     printf("\n");
+    
     for (i=0; i<6; i++){
         for (j=0; j<5; j++){
             printf("%d  ", mas[i][j]);
@@ -29,4 +29,3 @@ int main()
     printf("\n");
     }
 }
-
